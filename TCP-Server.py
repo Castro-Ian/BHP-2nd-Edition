6cb33625d45076a5decf92f95333c8ea6c82ac62
Ian C. <75397829+Castro-Ian@users.noreply.github.com>
2024-12-21 23:33:44 -0500

    Create TCP-Server.py
    This script demonstrates how to create a multi-threaded TCP server using Python's socket and threading libraries. The server listens for incoming connections, accepts them, and handles each client in a separate thread.

---
+++ TCP-Server.py
@@ -0,0 +1,22 @@
+import socket
+import threading
+
+IP ='0.0.0.0'
+PORT = 9998
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((IP, PORT))
+    server.listen(5)
+    print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
+
+def handle_client(client_socket):
+    with client_socket as a sock:
+        request = sock.recv(1024)
+        print(f'[*] Received: {request.decode("utf-8")}')
+        sock.send(b'ACK')
+
+if _name_ == '_main_':
+    main()
